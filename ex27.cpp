7900b652ddafbe6145202f5eb586ee6c9fac68d8
pROmeTEuL <radu.teodor.vatra@gmail.com>
2021-07-11 17:47:48 +0300

    Remedierea exercitiului 27.

--- ex27.cpp
+++ ex27.cpp
@@ -6,13 +6,13 @@ void ex27(){
     int r = 0;
     std::cout << "Scrie un numar: ";
     std::cin >> n;
-    for(int i; i <= n; i++){
+    for(int i = 0; i <= n; i++){
         if(i * i == n){
             r = i;
             std::cout << "Radical din acest numar este egal cu " << r << std::endl;
         }
     }
     if(r == 0){
-        std::cout << "Nu s-a putut calcula radical din acest numar deoarece acesta este prim.";
+        std::cout << "Nu s-a putut calcula radical din acest numar deoarece acesta este prim." << std::endl;
     }
 }
