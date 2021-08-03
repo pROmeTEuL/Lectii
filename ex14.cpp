597c3a31b40f4a7807c71ec4f128c661e4b92de3
pROmeTEuL <59443592+pROmeTEuL@users.noreply.github.com>
2021-08-03 17:05:06 +0300

    Update ex14.cpp

--- ex14.cpp
+++ ex14.cpp
@@ -15,9 +15,4 @@ void ex14()
     if (b > c) swap (b, c);
     if (a > b) swap (a, b);
     cout << a << ", " << b << ", " << c << endl;
-    int x;
-    cout << "Da un numar:";
-    cin >> x;
-    if (x%2==0) cout << "Numarul este par." << endl;
-    else cout << "Numarul este impar." << endl;
 }
